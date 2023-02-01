f82ff1828795be0749c5c3de3b20b77921bbce88
Dong <dong.nguyen@adamosoft.com>
2023-02-01 11:39:06 +0700

    add native victory for chart

--- src/containers/HomePage/Notification/Notification.tsx
+++ src/containers/HomePage/Notification/Notification.tsx
@@ -1,16 +1,83 @@
-import React from 'react';
-import {Text, Box, ScrollView} from 'native-base';
+import React, {useState} from 'react';
+import {View, ScrollView, StyleSheet} from 'react-native';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryContainer,
+  VictoryAxis,
+  Bar,
+  VictoryTooltip,
+} from 'victory-native';
 
 import {NotificationProps} from '.';
 
 const NotificationScreen = ({}: NotificationProps) => {
+  const [activeBar, setActiveBar] = useState(false);
   return (
     <ScrollView>
-      <Box>
-        <Text>Hello</Text>
-      </Box>
+      <View style={styles.container}>
+        <VictoryChart
+          domain={{x: [0, 5]}}
+          containerComponent={
+            <VictoryContainer disableContainerEvents={true} />
+          }>
+          <VictoryBar
+            barWidth={30}
+            cornerRadius={{top: 4}}
+            data={[
+              {x: 1, y: 2},
+              {x: 2, y: 4},
+              {x: 3, y: 1},
+              {x: 4, y: 5},
+            ]}
+            alignment="middle"
+            animate={{
+              duration: 500,
+              onLoad: {duration: 500},
+            }}
+            dataComponent={
+              <Bar
+                events={{onPress: () => setActiveBar(!activeBar)}}
+                style={{fill: activeBar ? '#ad1c6d' : '#E3F2FF'}}
+              />
+            }
+            labels={() => `RM ${String(Math.round(20))}`}
+            labelComponent={<VictoryTooltip active={activeBar} />}
+          />
+          <VictoryAxis
+            dependentAxis
+            style={{
+              axis: {
+                stroke: 'transparent',
+              },
+              tickLabels: {
+                fill: '#78849E',
+              },
+            }}
+          />
+          <VictoryAxis
+            style={{
+              axis: {
+                stroke: '#BDC5D8',
+              },
+              tickLabels: {
+                fill: '#78849E',
+              },
+            }}
+          />
+        </VictoryChart>
+      </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
 
 export default NotificationScreen;
